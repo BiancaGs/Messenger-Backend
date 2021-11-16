7fdc925293415b55050f2ed9b330cfa6302626cd
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-11-16 11:34:24 -0300

    Added route to search messages by word
    Co-authored-by: Bianca Gomes Rodrigues <BiancaGs@users.noreply.github.com>

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -1,12 +1,11 @@
-const Conversation = require('../models/Conversation');
+const mongoose = require('mongoose');
+const Conversation = require("../models/Conversation");
 
 module.exports = {
-
     /**
      * Find a conversation by its ObjectId
      */
     findById: async (req, res, next) => {
-
         const id = req.params.id;
 
         try {
@@ -15,19 +14,17 @@ module.exports = {
         } catch (error) {
             res.status(400).json(error);
         }
-
     },
 
     /**
      * Creates a new conversation
      */
     newConversation: async (req, res, next) => {
-
         const { title, members } = req.body;
 
         if (members.length <= 1) {
             res.status(400).json({
-                error: "You must add at least two members in a conversation"
+                error: "You must add at least two members in a conversation",
             });
         }
 
@@ -36,23 +33,20 @@ module.exports = {
                 title,
                 members,
                 messages: [],
-                created_at: new Date()
+                created_at: new Date(),
             });
             await newConversation.save();
 
             res.send(newConversation);
-
         } catch (error) {
             res.status(400).json(error);
         }
-
     },
 
     /**
      * Creates a new message in a specific conversation
      */
     newMessage: async (req, res, next) => {
-
         try {
             const id = req.params.id;
 
@@ -61,8 +55,8 @@ module.exports = {
             const newMessage = {
                 from,
                 body,
-                sent_at: new Date()
-            }
+                sent_at: new Date(),
+            };
 
             const conversation = await Conversation.findOne({ _id: id });
 
@@ -71,11 +65,49 @@ module.exports = {
             conversation.save();
 
             res.send(conversation);
-
         } catch (error) {
             res.status(400).json(error);
         }
+    },
 
-    }
+    /**
+     * Returns every message in the conversation the matchs a word
+     */
+    searchByWord: async (req, res, next) => {
+        const { id, word } = req.params;
 
-}
+        const wordRegex = new RegExp(word, "i");
+
+        try {
+            const agg = await Conversation.aggregate(
+                // Pipeline
+                [
+                    // Stage 1
+                    { $match: { _id: mongoose.Types.ObjectId(id) } },
+
+                    // Stage 2
+                    { $project: { messages: 1 } },
+
+                    // Stage 3
+                    { $unwind: { path: "$messages" } },
+
+                    // Stage 4
+                    {
+                        $addFields: {
+                            regexResult: {
+                                $regexFindAll: { input: "$messages.body", regex: wordRegex },
+                            },
+                        },
+                    },
+
+                    // Stage 5
+                    { $match: { regexResult: { $exists: true, $ne: [] } } },
+                ]
+            );
+
+            res.send(agg);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+    },
+};
--- routes/api/conversations.js
+++ routes/api/conversations.js
@@ -1,29 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const ConversationController = require('../../controllers/conversations');
+const ConversationController = require("../../controllers/conversations");
 
 /**
  * @route   GET /api/conversations/:id
  * @desc    Get the full conversation
  * @access  PUBLIC
  */
-router.route('/:id')
-    .get(ConversationController.findById);
+router.route("/:id").get(ConversationController.findById);
 
 /**
  * @route   POST /api/conversations/:id/new-message
  * @desc    Creates a new message on a conversation
  * @access  PUBLIC
  */
-router.route('/:id/new-message')
-    .post(ConversationController.newMessage);
+router.route("/:id/new-message").post(ConversationController.newMessage);
 
 /**
  * @route   POST /api/conversations
  * @desc    Creates a new conversation
  * @access  PUBLIC
  */
-router.route('/')
-    .post(ConversationController.newConversation);
+router.route("/").post(ConversationController.newConversation);
+
+/**
+ * @route   GET /api/conversations/:id/search/{word}
+ * @desc    Returns every message that matches a word
+ * @access  PUBLIC
+ */
+router.route("/:id/search/:word").get(ConversationController.searchByWord);
 
 module.exports = router;
