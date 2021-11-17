55c019be6a9650deb09f6953cf9ccb8a522849a7
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-11-16 23:20:18 -0300

    Teste de inserção de mensagens
    Co-authored-by: Bianca Gomes Rodrigues <BiancaGs@users.noreply.github.com>

--- scripts/insertMessage.js
+++ scripts/insertMessage.js
@@ -4,13 +4,13 @@ const User = require('../models/Conversation');
 const faker = require('faker');
 const axios = require('axios');
 
-let numberOfMessage = 1;
+let numberOfMessage = 100;
 var start = performance.now();
 
 for (let i = 0; i < numberOfMessage; i++) {
 
-    axios.post('http://localhost:5000/api/conversation/615d065bf49c478f67746e0e/new-message', {
-        "from": "615bb50482bbd4783e13ede2",
+    axios.post('http://localhost:5000/api/conversations/615d887dc0202898ed9e2b3d/new-message', {
+        "from": "615d8832c0202898ed9e2b3b",
         "body": faker.lorem.text()
     });
 
