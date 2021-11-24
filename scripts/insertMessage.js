6bdad8e5a581a9d40f5aea43167218af1d920654
Pietro Zuntini Bonfim <pietrozuntini@gmail.com>
2021-11-24 00:14:43 -0300

    Adjustments to the script to insert messages
    Co-authored-by: Bianca Gomes Rodrigues <BiancaGs@users.noreply.github.com>

--- scripts/insertMessage.js
+++ scripts/insertMessage.js
@@ -4,15 +4,23 @@ const User = require("../models/Conversation");
 const faker = require("faker");
 const axios = require("axios");
 
-let numberOfMessage = 20;
 var start = performance.now();
 
+let numberOfMessage = 20;
+let conversationId = "615d8965c0202898ed9e2b3e";
+
+let axiosCalls = [];
+
 for (let i = 0; i < numberOfMessage; i++) {
-    axios.post("http://localhost:5000/api/conversations/615d8965c0202898ed9e2b3e/new-message", {
-        from: "615d8832c0202898ed9e2b3b",
-        body: faker.lorem.text(),
-    });
+    axiosCalls.push(
+        axios.post(`http://localhost:5000/api/conversations/${conversationId}/new-message`, {
+            from: "615d8841c0202898ed9e2b3c",
+            body: faker.lorem.text(),
+        })
+    );
 }
 
-var end = performance.now();
-console.log(`Execution time: ${end - start} ms`);
+axios.all(axiosCalls).then(() => {
+    var end = performance.now();
+    console.log(`Execution time: ${end - start} ms`);
+});
